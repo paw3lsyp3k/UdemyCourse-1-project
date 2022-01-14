0fbf64efbbffdc81e9f79a6d46d95aa145059233
Paweł Sypek <paw3lsyp3k@gmail.com>
2022-01-14 22:05:42 +0100

    38TH episode

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExpenseDate.css'
+
+const ExpenseDate = ({date}) => {
+    const month = date.toLocaleString('pl-PL', {month: 'long'});
+    const day = date.toLocaleString('pl-PL', {day: '2-digit'});
+    const year = date.getFullYear();
+
+    return(
+        <div className='expense-date'>
+            <div className='expense-date__day'>{day}</div>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{year}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,14 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css'
 
 function ExpenseItem({title,date,amount}) {
-    const month = date.toLocaleString('pl-PL', {month: 'long'});
-    const day = date.toLocaleString('pl-PL', {day: '2-digit'});
-    const year = date.getFullYear();
+    
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{day}</div>
-                <div>{month}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
