9f72141f984c169634076f1e0d672f3f85c82b5e
Paweł Sypek <paw3lsyp3k@gmail.com>
2022-01-16 18:04:01 +0100

    xxx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css'
@@ -7,13 +8,13 @@ function ExpenseItem({title,date,amount}) {
     
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
